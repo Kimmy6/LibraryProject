c6902cb62ecc977d6602854f8fc120b0061d8eb8
Kimmie06 <kimms4579@gmail.com>
2021-08-24 17:14:39 +0900

    Safehouse

--- static/js/star.js
+++ static/js/star.js
@@ -1,9 +1,8 @@
 let score = document.getElementsByClassName('bookAvgscore')
-const onestar = "★"
 
 for (i=0; i<8; i++) {
     for (j=0; j<6; j++)
     if (score[i].innerText == j) {
-        score[i].innerText = "★".repeat(j)
+        score[i].innerText = "★".repeat(j) + "☆".repeat(5-j)
     }
 }
